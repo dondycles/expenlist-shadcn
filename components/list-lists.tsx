800bd5c644347af5fddf10fd2a18bfcc545a651a
dondycles <johnrod.dondoyano@gmail.com>
2024-01-05 02:15:01 +0800

    modified:   components/list-lists.tsx

--- components/list-lists.tsx
+++ components/list-lists.tsx
@@ -1,5 +1,7 @@
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Button } from "./ui/button";
+import { FaPencilAlt, FaTrash } from "react-icons/fa";
 
 const supabase = createServerComponentClient({ cookies });
 
@@ -10,9 +12,19 @@ export default async function ListLists() {
     <div className="flex flex-col h-full max-h-full gap-2 px-4 overflow-auto">
       {data?.map((expense) => {
         return (
-          <div className="w-full rounded-[0.5rem] grid grid-cols-2">
-            <p className="font-semibold text-primary">{expense.name}</p>
-            <p>{expense.cost}</p>
+          <div className="w-full rounded-[0.5rem] grid grid-cols-3 bg-primary/5">
+            <p className="flex items-center p-2 font-semibold text-primary">
+              {expense.name}
+            </p>
+            <p className="flex items-center p-2 ">{expense.cost}</p>
+            <div className="flex flex-row gap-2 ml-auto mr-0">
+              <Button variant={"outline"} size={"icon"}>
+                <FaPencilAlt />
+              </Button>
+              <Button variant={"destructive"} size={"icon"}>
+                <FaTrash />
+              </Button>
+            </div>
           </div>
         );
       })}
