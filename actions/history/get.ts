f230ca9d642cb86f72c306f273ecca5f11574f95
dondycles <johnrod.dondoyano@gmail.com>
2024-01-12 18:13:13 +0800

    modified:   actions/history/get.ts 	modified:   app/(user)/history/page.tsx

--- actions/history/get.ts
+++ actions/history/get.ts
@@ -2,12 +2,19 @@
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export const getHistory = async (range: string) => {
+export const getHistory = async ({
+  range,
+  last,
+}: {
+  range: string;
+  last?: string;
+}) => {
+  console.log(last);
   const supabase = createServerActionClient({ cookies });
   const { error, data } = await supabase
     .from("history")
     .select("*,expenses(*), user_data(*), savings(*)")
-    .order("created_at", { ascending: false })
+    .order("created_at", { ascending: last ? true : false })
     .range(
       Number(range) > 0 ? Number(range) - 10 : 0,
       range ? Number(range) : 10
--- app/(user)/history/page.tsx
+++ app/(user)/history/page.tsx
@@ -17,17 +17,23 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function History({
   searchParams,
 }: {
-  searchParams: { range: string };
+  searchParams: { range: string; last: string };
 }) {
-  const { data, isFetching } = useQuery({
+  const options = {
+    timeZone: "Asia/Manila",
+    hour12: false, // Use 24-hour format
+  };
+
+  const { data, isLoading } = useQuery({
     queryKey: ["history", searchParams.range],
-    queryFn: async () => getHistory(searchParams.range),
+    queryFn: async () =>
+      getHistory({ range: searchParams.range, last: searchParams.last }),
     refetchOnWindowFocus: false,
   });
 
   return (
     <div className="flex flex-col w-full h-full max-h-full gap-2 overflow-auto">
-      {isFetching ? (
+      {isLoading ? (
         <div className="flex flex-col h-full gap-2">
           <Skeleton className="flex flex-row w-full h-12 gap-1 p-1">
             <Skeleton className="flex-1 h-full bg-white/5" />
@@ -55,20 +61,14 @@ export default function History({
                 <TableHead>Cost/Amount</TableHead>
                 <TableHead>Deducted To</TableHead>
                 <TableHead>Total Savings</TableHead>
+                <TableHead>Date</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
               {data?.data &&
                 data?.data.map((history) => {
                   return (
-                    <TableRow
-                      id={history.id}
-                      // className={`
-                      // ${history.is_deleted && "bg-destructive/25"}
-                      // ${history.is_edit && "bg-secondary/25"}
-                      // ${!history.is_edit && !history.is_deleted && "bg-primary/25"}
-                      // `}
-                    >
+                    <TableRow id={history.id}>
                       <TableCell
                         className={`flex items-center justify-center `}
                       >
@@ -98,41 +98,86 @@ export default function History({
                       <TableCell>
                         {usePhpPeso(history.savings_overall_total)}
                       </TableCell>
+                      <TableCell>
+                        {new Date(String(history.created_at)).toLocaleString(
+                          "en-US",
+                          options
+                        )}
+                      </TableCell>
                     </TableRow>
                   );
                 })}
             </TableBody>
           </Table>
           <div className="flex flex-row justify-between gap-2 mt-auto mb-0">
-            {Number(searchParams.range) > 10 && (
+            {Number(searchParams.range) > 20 ? (
+              <div className="flex flex-row gap-2">
+                <Button asChild>
+                  <Link href={`/history?range=10`}>Back to first</Link>
+                </Button>
+                <Button asChild>
+                  <Link
+                    href={`/history?range=${
+                      Number(searchParams.range) === 10 ||
+                      Number(searchParams.range) === 0 ||
+                      !searchParams.range
+                        ? "10"
+                        : String(Number(searchParams.range) - 10)
+                    }&last=${searchParams.last === "true" ? "true" : "false"}`}
+                  >
+                    -10
+                  </Link>
+                </Button>
+              </div>
+            ) : (
+              <>
+                {Number(searchParams.range) > 10 && (
+                  <Button asChild>
+                    <Link
+                      href={`/history?range=${
+                        Number(searchParams.range) === 10 ||
+                        Number(searchParams.range) === 0 ||
+                        !searchParams.range
+                          ? "10"
+                          : String(Number(searchParams.range) - 10)
+                      }&last=${
+                        searchParams.last === "true" ? "true" : "false"
+                      }`}
+                    >
+                      -10
+                    </Link>
+                  </Button>
+                )}
+              </>
+            )}
+
+            <p>Last 10 History</p>
+            <div className="flex flex-row gap-2">
               <Button asChild>
                 <Link
-                  href={`/history?range=${
-                    Number(searchParams.range) === 10 ||
-                    Number(searchParams.range) === 0 ||
-                    !searchParams.range
-                      ? "10"
-                      : String(Number(searchParams.range) - 10)
-                  }`}
-                >
-                  -10
-                </Link>
-              </Button>
-            )}
-            <p>Last 10 History</p>
-            <Button asChild>
-              <Link
-                href={`
+                  href={`
               /history?range=${
                 searchParams.range
                   ? String(Number(searchParams.range) + 10)
                   : "20"
-              }
+              }&last=${searchParams.last === "true" ? "true" : "false"}
             `}
-              >
-                +10
-              </Link>
-            </Button>
+                >
+                  +10
+                </Link>
+              </Button>
+              {searchParams.last === "false" && (
+                <Button asChild>
+                  <Link
+                    href={`
+              /history?range=10&last=true
+            `}
+                  >
+                    From the last
+                  </Link>
+                </Button>
+              )}
+            </div>
           </div>
         </>
       )}
